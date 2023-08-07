3dfb0d7d6f234d5f0eff923327de7c6017970beb
20120206 <nguyenngocthuy9a1@gmail.com>
2023-08-07 21:27:30 +0700

    set some styles for wrapper and container

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,18 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <h2 className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div>Header</div>
+                <div>Header</div>
+                <div>Header</div>
+            </div>
+        </h2>
+    );
 }
 
 export default Header;
